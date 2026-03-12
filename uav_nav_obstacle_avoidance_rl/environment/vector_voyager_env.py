7d16407711572cdb813d01e485c5bb11808509bf
anastasios <163117674+anastasi0s@users.noreply.github.com>
2026-03-12 01:20:31 +0100

    no walls only out of bounds

--- uav_nav_obstacle_avoidance_rl/environment/vector_voyager_env.py
+++ uav_nav_obstacle_avoidance_rl/environment/vector_voyager_env.py
@@ -156,9 +156,9 @@ class VectorVoyagerEnv(QuadXBaseEnv):
                 os.dup2(old_fd, 1)
                 os.close(old_fd)
 
-        # --- create new boundary walls
-        self.boundary_wall_ids.clear()
-        self._create_boundary_walls()
+        # # --- create new boundary walls
+        # self.boundary_wall_ids.clear()
+        # self._create_boundary_walls()
 
         # --- reset waypoint handler, which sets the current target -> create targets manually from voxel grid and overwrite the targets attribute
         self.waypoints.reset(self.env, self.np_random)
@@ -233,21 +233,20 @@ class VectorVoyagerEnv(QuadXBaseEnv):
             self.info["collision"] = True
             self.termination |= True
  
-        # ## OOB limits are INVISIBLE to the agent!! -> use walls that prevent out of bounds and collisions with them are detectable 
-        # # check exceed rectangular bounds on cartesian grid_sizes
-        # lin_pos = self.env.state(0)[-1]  # get current position (lin_pos is at state[3])
-        # x, y, z = lin_pos  # [x, y, z]
+        ## OOB limits are INVISIBLE to the agent!! -> use walls that prevent out of bounds and collisions with them are detectable 
+        # check exceed rectangular bounds on cartesian grid_sizes
+        lin_pos = self.env.state(0)[-1]  # get current position (lin_pos is at state[3])
+        x, y, z = lin_pos  # [x, y, z]
 
-        # if (
-        #     x < self.occupancy_grid.x_min
-        #     or x > self.occupancy_grid.x_max
-        #     or y < self.occupancy_grid.y_min
-        #     or y > self.occupancy_grid.y_max
-        #     or z > self.z_size  # z constrains the max hight the uav is allowed to fly. collision with the floor is checked above already!
-        # ):
-        #     # self.reward = -100.0
-        #     self.info["out_of_bounds"] = True
-        #     self.termination |= True
+        if (
+            x < self.occupancy_grid.x_min
+            or x > self.occupancy_grid.x_max
+            or y < self.occupancy_grid.y_min
+            or y > self.occupancy_grid.y_max
+            or z > self.z_size  # z constrains the max hight the uav is allowed to fly. collision with the floor is checked above already!
+        ):
+            self.info["out_of_bounds"] = True
+            self.termination |= True
 
         # ── per-sub-step dense shaping data ──
         self.info["sub_progress"].append(float(self.waypoints.progress_to_next_target))

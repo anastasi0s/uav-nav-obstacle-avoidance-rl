35d2e17f12fef5e9b6104ea6401d593614b424ce
anastasios <163117674+anastasi0s@users.noreply.github.com>
2026-03-13 19:38:43 +0100

    exp5 lidar only normalization

--- uav_nav_obstacle_avoidance_rl/utils/env_factory.py
+++ uav_nav_obstacle_avoidance_rl/utils/env_factory.py
@@ -55,7 +55,7 @@ def make_flat_voyager(**env_kwargs):
         env = CustomRewardWrapper(env, **reward_kwargs)
 
     # 4. normalization wrappers - normalize actions and observations (works with and without lidar)
-    env = RescaleAction(env, min_action=-1.0, max_action=1.0)
+    # env = RescaleAction(env, min_action=-1.0, max_action=1.0)
     env = NormalizeObservationWrapper(env)
     
     # 5. flatten env
